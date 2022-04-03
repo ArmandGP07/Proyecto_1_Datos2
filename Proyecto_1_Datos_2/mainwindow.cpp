8e30c3bcb33be76f3a435b88fbbe6c0a40bb2521
Armando García Paniagua <armandgp07@gmail.com>
2022-04-03 00:31:00 -0600

    Funcionalidad de cronometro
    Se implementó funcionalidad del cronómetro para que se actualice correctamente

--- Proyecto_1_Datos_2/mainwindow.cpp
+++ Proyecto_1_Datos_2/mainwindow.cpp
@@ -1,13 +1,23 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+
+//Constructor implementado//
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    connect(timer, SIGNAL (timeout ()), this, SLOT(actualizarEstado()));
+
 }
 
+void MainWindow::actualizarEstado(){
+    actualizarCronometro ();
+    definirResultadoFinal();
+}
+
+//Destructor implementado//
 MainWindow::~MainWindow()
 {
     delete ui;
--- Proyecto_1_Datos_2/mainwindow.h
+++ Proyecto_1_Datos_2/mainwindow.h
@@ -2,20 +2,36 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QTimer>
+#include <QTime>
 
-QT_BEGIN_NAMESPACE
-namespace Ui { class MainWindow; }
-QT_END_NAMESPACE
+//Clase de la ventana principal //
+namespace Ui {
+class MainWindow;
+}
 
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
 
 public:
+    //Constructor//
     MainWindow(QWidget *parent = nullptr);
+    //Destructor//
     ~MainWindow();
+    //Temporizador//
+    QTimer *timer = new QTimer();
+    //Reloj//
+    QTime time;
+
+//Modificador de acceso para cronómetro funcional//
+private slots:
+   void actualizarEstado();
+   void actualizarCronometro();
+   void definirResultadoFinal();
 
 private:
+    //Objeto que controla la interfaz gráfica//
     Ui::MainWindow *ui;
 };
 #endif // MAINWINDOW_H
